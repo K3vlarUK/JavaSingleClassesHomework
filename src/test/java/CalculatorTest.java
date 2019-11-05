03aed8262c13597943425fc2fd200e87b48ce0ac
Kevin McDermott <kmcdermott31@gmail.com>
2019-11-05 09:37:04 +0000

    Homework Complete - Fixed delta in calculator tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -14,21 +14,21 @@ public class CalculatorTest {
 
     @Test
     public void testAdd(){
-        assertEquals(5.0, calculator.add(2.0,3.0));
+        assertEquals(5.0, calculator.add(2.0,3.0), 0.01);
     }
 
     @Test
     public void testSubtract(){
-        assertEquals(5.0, calculator.subtract(10.0,5.0));
+        assertEquals(5.0, calculator.subtract(10.0,5.0), 0.01);
     }
 
     @Test
     public void testMultiply(){
-        assertEquals(15.0, calculator.multiply(3.0, 5.0));
+        assertEquals(15.0, calculator.multiply(3.0, 5.0), 0.01);
     }
 
     @Test
     public void testDivide(){
-        assertEquals(3.0, calculator.divide(9.0, 3.0));
+        assertEquals(3.0, calculator.divide(9.0, 3.0), 0.01);
     }
 }
